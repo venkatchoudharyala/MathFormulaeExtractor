9e272e36725eb5142af9b5ad16eca73d1cc15319
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2023-12-28 23:44:58 +0530

    Update App.py

--- App.py
+++ App.py
@@ -34,65 +34,57 @@ st.title("Math Formulae Extractor")
 st.write("Leverage the Power of Gemini, GPT-4 and extract Maths Formulae from Images....")
 st.write("---")
 
-ModelName = st.selectbox("Select a Model", ("GPT-4-vision-preview", "gemini-pro-vision"))
+ModelName = st.selectbox("Select a Model", ("gemini-pro-vision", "gpt-4-vision-preview"))
 
 Image = st.file_uploader("Upload your Image!!")
 
 if Image:
         st.image(Image)
+	
+def ChatGPT(Image):
+	buffer = io.BytesIO(Image.read())
+        base64_encoded_image = base64.b64encode(buffer.read()).decode("utf-8")
+        data = f"data:image/jpeg;base64,{base64_encoded_image}"
+	APIKey = st.text_input("Enter your GPT-4 API Key")
+        client = OpenAI(api_key = APIKey)
+	with st.spinner("We'r Almost there!!!"):
+		response = client.chat.completions.create(
+			model="gpt-4-vision-preview",
+			messages=[
+			  {
+			    "role": "user",
+			    "content": [
+			      {"type": "text", "text": "Hey Gemini, Extract Mathematical formulae from this Image and convert that into LaTeX Text. Remember dont include any text other than formulae and put each formula line by line if it has multiple formulae."},
+			      {
+				"type": "image_url",
+				"image_url": {
+				"url": data,
+				},
+			      },
+			    ],
+			  }
+			],
+			max_tokens=300,
+		      )
+        	st.markdown(response.choices[0].message.content)
+        	btn = st.download_button(label = "Download File", data = response.choices[0].message.content, file_name = "Files/New.tex")
 
-def API(ModelName):
-        if ModelName == "gemini-vision-pro":
-                genai.configure(api_key='AIzaSyBE1HLZuDQHbVz1C6MPD9FcvPbkeJqGrQU')
-                model = genai.GenerativeModel('gemini-pro-vision')
-                return model
-        elif ModelName == "GPT-4-vision-preview":
-                APIKey = st.text_input("Enter your GPT-4 API Key")
-                client = OpenAI(api_key = APIKey)
-                return client
-
-model = API(ModelName)
-
-def Extractor(img, ModelName):
-        if ModelName == "gemini-vision-pro":
-                response = model.generate_content(["Hey Gemini, Extract Mathematical formulae from this Image and convert that into LaTeX Text.", img], stream=True)
+def GeminiAI(Image):
+	genai.configure(api_key='AIzaSyBE1HLZuDQHbVz1C6MPD9FcvPbkeJqGrQU')
+        model = genai.GenerativeModel('gemini-pro-vision')
+	image = PIL.Image.open(Image)
+        with st.spinner("We'r Almost there!!!"):
+		response = model.generate_content(["Hey Gemini, Extract Mathematical formulae from this Image and convert that into LaTeX Text.", img], stream=True)
                 response.resolve()
 
                 st.write(to_markdown(response.text))
 
                 btn = st.download_button(label = "Download File", data = response.text, file_name = "Files/New.tex")
-        elif ModelName == "GPT-4-vision-preview":
-                #url = "https://i.ytimg.com/vi/fk81g5c6PNQ/maxresdefault.jpg"
-                response = model.chat.completions.create(
-                                model="gpt-4-vision-preview",
-                                messages=[
-                                  {
-                                    "role": "user",
-                                    "content": [
-                                      {"type": "text", "text": "Hey Gemini, Extract Mathematical formulae from this Image and convert that into LaTeX Text. Remember dont include any text other than formulae and put each formula line by line if it has multiple formulae."},
-                                      {
-                                        "type": "image_url",
-                                        "image_url": {
-                                        "url": img,
-                                        },
-                                      },
-                                    ],
-                                  }
-                                ],
-                                max_tokens=300,
-                              )
-                st.markdown(response.choices[0].message.content)
-                btn = st.download_button(label = "Download File", data = response.choices[0].message.content, file_name = "Files/New.tex")
-if Image and st.button("Extract"):
-        if ModelName == "gemini-vision-pro":
-                image = PIL.Image.open(Image)
-                with st.spinner("We'r Almost there!!!"):
-                        Extractor(image, ModelName)
-        elif ModelName == "GPT-4-vision-preview":
-                buffer = io.BytesIO(Image.read())
-                base64_encoded_image = base64.b64encode(buffer.read()).decode("utf-8")
-                data = f"data:image/jpeg;base64,{base64_encoded_image}"
-                with st.spinner("We'r Almost there!!!"):
-                        Extractor(data, ModelName)
 
-        
+def main():
+	if ModelName == "gpt-4-vision-preview" and Image:
+		ChatGPT(Image)
+	elif ModelName == "gemini-vision-pro" and Image:
+		GeminiAI(Image)
+if __name__ == "__main__":
+    main()
