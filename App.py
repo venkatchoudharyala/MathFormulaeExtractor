3b6207692d1a40c51920bb19c5ff106747f2cead
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2023-12-28 23:00:21 +0530

    Update App.py

--- App.py
+++ App.py
@@ -56,6 +56,8 @@ def APIConfig(ModelName):
                 client = OpenAI(api_key = APIKey)
                 return client
 
+model = APIConfig(ModelName)
+
 def Extractor(img, ModelName):
         if ModelName == "gemini-vision-pro":
                 response = model.generate_content(["Hey Gemini, Extract Mathematical formulae from this Image and convert that into LaTeX Text.", img], stream=True)
@@ -88,11 +90,9 @@ def Extractor(img, ModelName):
                 btn = st.download_button(label = "Download File", data = response.choices[0].message.content, file_name = "Files/New.tex")
 if Image and st.button("Extract"):
         if ModelName == "gemini-vision-pro":
-                model = APIConfig(ModelName)
                 with st.spinner("We'r Almost there!!!"):
                         Extractor(img, ModelName)
         elif ModelName == "GPT-4-vision-preview":
-                model = APIConfig(ModelName)
                 buffer = io.BytesIO(Image.read())
                 base64_encoded_image = base64.b64encode(buffer.read()).decode("utf-8")
                 data = f"data:image/jpeg;base64,{base64_encoded_image}"
