501697ebdb2a43a8321cba4e1d24d00d2d3eb268
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-02-03 19:28:37 +0530

    Update App.py

--- App.py
+++ App.py
@@ -80,9 +80,9 @@ def GeminiAI(Image):
 	genai.configure(api_key='AIzaSyB8ayw3zz3HuZDPYJuyS4rYUcnj8cH28XI')
 	model = genai.GenerativeModel('gemini-pro-vision')
 	image = PIL.Image.open(Image)
-	st.image(image)
 	if st.button("Extract"):
 		with st.spinner("We'r Almost there!!!"):
+			print("1")
 			response = model.generate_content([Prompt, image], stream=True)
 			response.resolve()
 			k = to_markdown(response.text)
